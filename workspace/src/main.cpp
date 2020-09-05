ca6e95d139d4ca1c9adc2c1c6191df253d22ade6
Philippe FOUBERT <philippe.foubert@free.fr>
2020-09-05 18:08:53 +0200

    Update the example

--- workspace/src/main.cpp
+++ workspace/src/main.cpp
@@ -1,22 +1,20 @@
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include <iostream>
-using namespace cv;
-int main()
-{
-    std::string image_path = samples::findFile("HappyFish.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-        imwrite("HappyFish.jpg", img);
-    }
-    return 0;
+
+int main() {
+  std::string image_path = cv::samples::findFile("HappyFish.jpg");
+  cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+  if(img.empty()) {
+    std::cerr << "Could not read the image: " << image_path << std::endl;
+    return -1;
+  }
+  cv::imshow("Display window", img);
+  auto k = static_cast<char>(cv::waitKey(0));  // Wait for a keystroke in the window
+  if ('s' == k) {
+    cv::imwrite("HappyFish.jpg", img);
+  }
+  return 0;
 }
+
